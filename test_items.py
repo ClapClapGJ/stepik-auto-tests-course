3b737c8a8bc599ace1a04fa91a029c20d3804464
ClapClapGJ <80744587+ClapClapGJ@users.noreply.github.com>
2021-07-18 22:02:13 +0300

    Delete test_items.py
    no need

--- test_items.py
+++
@@ -1,8 +0,0 @@
-
-class TestLang:
-    def test_button_for_item_add(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-        browser.get(link)
-        browser.implicitly_wait(10)
-        button = len(browser.find_elements_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"))
-        assert button > 0, '!!!НЕ НАШЕЛ!!!'
